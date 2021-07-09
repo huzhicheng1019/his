2b79ed16a173aa8046dbd94d63cccc76777398b0
lka598ytut <2291285901@qq.com>
2021-07-09 11:15:20 +0800

    修改

--- src/main/java/cn/gson/his/model/mappers/lxj/RoleMapper.java
+++ src/main/java/cn/gson/his/model/mappers/lxj/RoleMapper.java
@@ -1,5 +1,6 @@
 package cn.gson.his.model.mappers.lxj;
 
+import cn.gson.his.model.pojos.lxj.Dept;
 import cn.gson.his.model.pojos.lxj.RoleInfo;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -10,4 +11,6 @@ import java.util.Map;
 public interface RoleMapper {
 
     public List<Map<String,Object>> allRole();
+
+    public List<Dept> allRoleDept(Integer id);
 }
