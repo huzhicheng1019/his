a6c607d3223e515c15f8d096afcbd84586c991ca
llw <1580958661@qq.com>
2021-07-30 15:13:37 +0800

    llw12

---
+++ src/main/java/cn/gson/his/model/pojos/Drug/Allot.java
@@ -0,0 +1,91 @@
+package cn.gson.his.model.pojos.Drug;
+
+import cn.gson.his.model.pojos.Power.Employee;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+public class Allot {
+    private Integer allotId;
+    private Timestamp allotDate;
+    private Timestamp sqdate;
+    private Employee employee;
+    private LibraryInfoEntity library;//调入仓库
+    private LibraryInfoEntity libraryto;//调离仓库
+
+    @Id
+    @Column(name = "ALLOT_ID", nullable = false, precision = 0)
+    public Integer getAllotId() {
+        return allotId;
+    }
+
+    public void setAllotId(Integer allotId) {
+        this.allotId = allotId;
+    }
+
+    @Basic
+    @Column(name = "ALLOT_DATE", nullable = true)
+    public Timestamp getAllotDate() {
+        return allotDate;
+    }
+
+    public void setAllotDate(Timestamp allotDate) {
+        this.allotDate = allotDate;
+    }
+
+    @Basic
+    @Column(name = "SQDATE", nullable = true)
+    public Timestamp getSqdate() {
+        return sqdate;
+    }
+
+    public void setSqdate(Timestamp sqdate) {
+        this.sqdate = sqdate;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "ZXR", referencedColumnName = "EMP_ID")
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+
+    @ManyToOne
+    @JoinColumn(name = "LIBRARYTO_ID", referencedColumnName = "LIBRARY_ID")
+    public LibraryInfoEntity getLibraryto() {
+        return libraryto;
+    }
+
+    public void setLibraryto(LibraryInfoEntity libraryto) {
+        this.libraryto = libraryto;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "LIBRARY_ID", referencedColumnName = "LIBRARY_ID")
+    public LibraryInfoEntity getLibrary() {
+        return library;
+    }
+
+    public void setLibrary(LibraryInfoEntity library) {
+        this.library = library;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Allot allot = (Allot) o;
+        return Objects.equals(allotId, allot.allotId) && Objects.equals(allotDate, allot.allotDate) && Objects.equals(sqdate, allot.sqdate) && Objects.equals(libraryto, allot.libraryto) && Objects.equals(library, allot.library);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(allotId, allotDate, sqdate, employee, libraryto, library);
+    }
+}
